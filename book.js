129d5ff3a7599b0ad77f9346b15b4e102004876e
Colin <me@colinmcglone.ca>
2021-07-21 14:31:42 -0400

    t

---
+++ book.js
@@ -0,0 +1,30 @@
+/* Expense Document
+{
+    "id": int,
+    "vendor": "McDonalds",
+    "amount": float,
+    "purchaser": "Alice",
+    "settled": bool,
+    "date": date,
+    "description": description,
+}
+*/
+
+/* Book Document
+{
+    "id": book,
+    "expenses": [int, int, int],
+    "status": open,
+    "participants": [
+        {
+            "name": "Alice",
+            "balance": float
+        },
+        {
+            "name": "Bob",
+            "balance": float
+        }
+    ],
+    "history": [expense, expense, settlement]
+}
+*/
